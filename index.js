77758087fe752a1e49be293021fa2f33d627eb19
Akshay Patel <Akshaysukhadiya41@gmail.com>
2024-02-26 10:54:41 -0800

    Assignment 2

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const https = require('https')
 const fs = require('fs');
 const startup = require('./routes/startup');
-const classSchedule = require('./routes/classSchedule');
 
 const app = express();
 // const cert = fs.readFileSync('./ssl/cert.pem')
@@ -16,7 +15,6 @@ const server = https.createServer(httpsOptions, app);
 app.use(express.json())
 
 app.use('/https-web-service/v1', startup)
-app.use('/https-web-service/v1', classSchedule)
 //domain-name/seb-service/v1/<route/path/endpoint> ==> endpoint
 //safeway.com/order-purchases/v1/purchaseHistory => endpoint
 //safeway.com/order-purchases/v2/purchaseHistory => endpoint
