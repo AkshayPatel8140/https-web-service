635c4741febe05e3e546c672a821129e60d0813a
Akshay Patel <Akshaysukhadiya41@gmail.com>
2024-03-01 15:19:19 -0800

    assignment 3

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const https = require('https')
 const fs = require('fs');
-const startup = require('./routes/startup');
 const students = require('./routes/students');
 
 const app = express();
